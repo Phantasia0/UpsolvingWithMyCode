80e510c49db072b54ff652016e61e67dd31e7143
Phantasia0 <ncprog1@gmail.com>
2023-02-25 03:19:35 +0900

    Greedy 문제 같지만 Greedy 아닌 문제임. <<<

---
+++ Collections/Greedy/씨름 선수/solution.js
@@ -0,0 +1,48 @@
+const input = require("fs")
+  .readFileSync("input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = parseInt(input[0]);
+const arr = Array.from(Array(5), () => []);
+for (let i = 0; i < n; i++) {
+  arr[i] = Array.from(input[i + 1].trim().split(" ")).map((v) => parseInt(v));
+}
+
+let cnt = 0;
+let res = 0;
+//const list = [];
+
+function DFS(s, weight) {
+  if (s < 0) return;
+
+  if (cnt + n - s <= res) {
+    cnt -= 1;
+    s -= 1;
+    return;
+  }
+
+  for (let i = s; i < n; i++) {
+    if (arr[i][1] > weight) {
+      cnt += 1;
+      //list.push(arr[i][1]);
+      DFS(i + 1, arr[i][1]);
+      //list.pop();
+    }
+  }
+
+  res = Math.max(res, cnt);
+  //console.log(list);
+  cnt -= 1;
+}
+
+function solution(n, arr) {
+  arr.sort((a, b) => b[0] - a[0]);
+
+  DFS(0, 0);
+
+  console.log(res);
+}
+
+solution(n, arr);
---
+++ Collections/Greedy/씨름 선수/solution2.js
@@ -0,0 +1,39 @@
+const input = require("fs")
+  .readFileSync("input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = parseInt(input[0]);
+const arr = Array.from(Array(5), () => []);
+for (let i = 0; i < n; i++) {
+  arr[i] = Array.from(input[i + 1].trim().split(" ")).map((v) => parseInt(v));
+}
+
+function solution(n, arr) {
+  arr.sort((a, b) => b[0] - a[0]);
+
+  let cnt = 0;
+  let largest = 0;
+  let res = 0;
+  for (let i = 0; i < n; i++) {
+    largest = arr[i][1];
+    cnt += 1;
+    if (res >= n - i) {
+      break;
+    }
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j][1] > largest) {
+        largest = arr[j][1];
+        cnt += 1;
+      }
+    }
+    res = Math.max(res, cnt);
+
+    cnt = 0;
+  }
+
+  console.log(res);
+}
+
+solution(n, arr);
