087cd830145ab9261c97b43a47c5cd9d009710e9
Cat Hanbit <ncprog1@gmail.com>
2023-03-02 03:34:18 +0900

    highest top easy version

--- Collections/DynamicProgramming/가장 높은 탑 쌓기/solution.js
+++ Collections/DynamicProgramming/가장 높은 탑 쌓기/solution.js
@@ -9,10 +9,30 @@ const arr = [];
 for (let i = 0; i < n; i++) {
   arr[i] = Array.from(input[i + 1].trim().split(" ")).map((v) => parseInt(v));
 }
+arr.unshift([0, 0, 0]);
 
 function solution(n, arr) {
-    
+  arr.sort((a, b) => a[0] - b[0]);
+  const cv = arr.map((v) => v[2]);
+  const dp = Array(n + 1).fill(0);
+  dp[1] = arr[1][1];
+  let res = 0;
 
+  for (let i = 2; i < n + 1; i++) {
+    let max = 0;
+    for (let j = i - 1; j > 0; j--) {
+      if (cv[i] > cv[j] && dp[j] > max) {
+        max = dp[j];
+      }
+    }
+    dp[i] = max + arr[i][1];
+
+    if (dp[i] > res) {
+      res = dp[i];
+    }
+  }
+
+  console.log(res);
 }
 
 solution(n, arr);
